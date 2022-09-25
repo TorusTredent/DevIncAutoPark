2000c56e3b43ce2ad819c6eb141bf24c455c7210
TorusTredent <torus.tredent@gmail.com>
2022-09-25 20:49:27 +0300

    + complete level1

--- src/by/incubator/Main.java
+++ src/by/incubator/Main.java
@@ -1,7 +1,56 @@
 package by.incubator;
 
+import by.incubator.level1.VehicleType;
+
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        startLevel1();
+    }
+
+    private static void startLevel1() {
+        VehicleType[] vehicleType = {
+                new VehicleType("Bus", 1.2),
+                new VehicleType("Car", 1),
+                new VehicleType("Rink", 1.5),
+                new VehicleType("Tractor", 1.2),
+        };
+
+        for (VehicleType type : vehicleType) {
+            type.display();
+        }
+
+        vehicleType[vehicleType.length - 1].setRoadTaxCoefficient(1.3);
+
+        double maxTax = Arrays.stream(vehicleType)
+                .mapToDouble(VehicleType::getRoadTaxCoefficient)
+                .filter(type -> type >= 0)
+                .max()
+                .orElse(0);
+
+        double allTax = Arrays.stream(vehicleType)
+                .mapToDouble(VehicleType::getRoadTaxCoefficient)
+                .sum();
+
+        double averageTax = allTax/vehicleType.length;
+
+
+
+        maxTax = 0;
+        averageTax = 0;
+        allTax = 0;
+        for (int i = 0; i < vehicleType.length; i++) {
+            if (maxTax < vehicleType[i].getRoadTaxCoefficient()) {
+                maxTax = vehicleType[i].getRoadTaxCoefficient();
+            }
+
+            allTax += vehicleType[i].getRoadTaxCoefficient();
+            if (i == vehicleType.length - 1) {
+                averageTax = allTax / vehicleType.length;
+            }
+
+            vehicleType[i].display();
+        }
     }
 }
---
+++ src/by/incubator/level1/VehicleType.java
@@ -0,0 +1,40 @@
+package by.incubator.level1;
+
+public class VehicleType {
+
+    private String name;
+    private double roadTaxCoefficient;
+
+    public VehicleType() {
+    }
+
+    public VehicleType(String name, double roadTaxCoefficient) {
+        this.name = name;
+        this.roadTaxCoefficient = roadTaxCoefficient;
+    }
+
+    public void display() {
+        System.out.println( "name = " + name + "\n" + "roadTaxCoefficient = " + roadTaxCoefficient);
+    }
+
+    public void getString() {
+        System.out.println(name + "," + "'" + roadTaxCoefficient + "'");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getRoadTaxCoefficient() {
+        return roadTaxCoefficient;
+    }
+
+    public void setRoadTaxCoefficient(double roadTaxCoefficient) {
+        this.roadTaxCoefficient = roadTaxCoefficient;
+    }
+
+}
