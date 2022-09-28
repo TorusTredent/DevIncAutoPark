fe39bea90ab464d66475f9615b4541ad86a5fb79
TorusTredent <torus.tredent@gmail.com>
2022-09-28 17:30:39 +0300

    change getString(), refact Main

--- src/by/incubator/Main.java
+++ src/by/incubator/Main.java
@@ -6,37 +6,24 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        startLevel1();
+        VehicleType[] vehicleTypes = startLevel1();
     }
 
-    private static void startLevel1() {
-        VehicleType[] vehicleType = {
-                new VehicleType("Bus", 1.2),
-                new VehicleType("Car", 1),
-                new VehicleType("Rink", 1.5),
-                new VehicleType("Tractor", 1.2),
-        };
-
+    private static VehicleType[] startLevel1() {
+        VehicleType[] vehicleType = initVehicleType();
         for (VehicleType type : vehicleType) {
             type.display();
         }
-
         vehicleType[vehicleType.length - 1].setRoadTaxCoefficient(1.3);
-
         double maxTax = Arrays.stream(vehicleType)
                 .mapToDouble(VehicleType::getRoadTaxCoefficient)
                 .filter(type -> type >= 0)
                 .max()
                 .orElse(0);
-
         double allTax = Arrays.stream(vehicleType)
                 .mapToDouble(VehicleType::getRoadTaxCoefficient)
                 .sum();
-
-        double averageTax = allTax/vehicleType.length;
-
-
-
+        double averageTax = allTax / vehicleType.length;
         maxTax = 0;
         averageTax = 0;
         allTax = 0;
@@ -44,13 +31,22 @@ public class Main {
             if (maxTax < vehicleType[i].getRoadTaxCoefficient()) {
                 maxTax = vehicleType[i].getRoadTaxCoefficient();
             }
-
             allTax += vehicleType[i].getRoadTaxCoefficient();
             if (i == vehicleType.length - 1) {
                 averageTax = allTax / vehicleType.length;
             }
-
             vehicleType[i].display();
         }
+        return vehicleType;
+    }
+
+
+    private static VehicleType[] initVehicleType() {
+        return new VehicleType[]{
+                new VehicleType("Bus", 1.2),
+                new VehicleType("Car", 1),
+                new VehicleType("Rink", 1.5),
+                new VehicleType("Tractor", 1.2),
+        };
     }
 }
