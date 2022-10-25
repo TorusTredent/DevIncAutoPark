618803086e3a699fe845156654468f5bf2b768b2
TorusTredent <torus.tredent@gmail.com>
2022-10-25 14:05:33 +0300

    complete lvl6

--- src/by/incubator/console/Writer.java
+++ src/by/incubator/console/Writer.java
@@ -1,5 +1,7 @@
 package by.incubator.console;
 
+import by.incubator.entity.vehicle.Vehicle;
+
 public class Writer {
 
     public static <T> void print(T text) {
@@ -14,5 +16,20 @@ public class Writer {
 
     public static <T> void printError(T text) {
         System.err.println(text);
+    }
+
+    public static void printfHeader(String text) {
+        System.out.printf(text, "Id", "Type", "ModelName", "Number", "Weight (kg)",
+                "Year", "Mileage", "Color", "Income", "Tax", "Profit");
+    }
+
+    public static void printfVehiclesInLine(String text, Vehicle vehicle) {
+        System.out.format(text, vehicle.getId(), vehicle.getVehicleType().getName(), vehicle.getModelName(),
+                vehicle.getRegNumber(), vehicle.getWeight(), vehicle.getManufactureYear(), vehicle.getMileage(),
+                vehicle.getColor(), vehicle.getTotallIncom(), vehicle.getCalcTaxPerMonth(), vehicle.getTotalProfit());
+    }
+
+    public static void printfTotal(String textTotal, double sumTotalProfit) {
+        System.out.printf(textTotal, "Total", sumTotalProfit);
     }
 }
